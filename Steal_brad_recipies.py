28c4efba5279cf9c9f20a25b768061e61e7ab73f
Dmitry Petlin <DimonRonD@gmail.com>
2025-07-17 15:46:28 +0300

    New test file

--- Steal_brad_recipies.py
+++ Steal_brad_recipies.py
@@ -2,25 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 # fetch the page content
-url = 'https://lafoy.ru/keto-hleb-recepty-3923'
+url = 'https://lafoy.ru/makarony-s-syrom-v-duhovke-recepty-4384'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup.prettify())
 
 # extract product names, prices, and image URLs
 bred_names = soup.find_all('h2', class_='tm8 post__title')
 bred_ingridients = soup.find_all('p', attrs={'class': 'tm11 post__text'})
-#bred_recipies = soup.find_all_next('p', class_='tm11 post__text')
 
 counter = 0
+biggers = ["1."] #, "2.", "4.", "6.", "7.", "8."]
 for name in bred_names:
-    print(f"Product: {name.text.strip()}")
-    print(f"Recipie: {bred_ingridients[counter].text.strip()}")
-    print(f"Recipie: {bred_ingridients[counter + 1].text.strip()}")
-    if name.text.strip().startswith('8'):
-        print(f"Recipie: {bred_ingridients[counter + 2].text.strip()}")
-        counter += 1
+    print(f"Название: {name.text.strip()}")
+    print(f"Ингридиенты: {bred_ingridients[counter].text.strip()}")
+    print(f"Рецепт: {bred_ingridients[counter + 1].text.strip()}")
+    for i in biggers:
+        if name.text.strip().startswith(i):
+            print(f"{bred_ingridients[counter + 2].text.strip()}")
+            counter += 1
     print("-" * 35)
     counter += 2
 
