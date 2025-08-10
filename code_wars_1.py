a069ff57f8cefc71b1fb575f799d77e63882ccbe
Dmitry Petlin <DimonRonD@gmail.com>
2025-08-10 11:54:02 +0300

    New test file

--- code_wars_1.py
+++ code_wars_1.py
@@ -46,35 +46,93 @@ from eduson_git_test import count
 # print(accum("ZpglnRxqenU"))
 ##########################################################################
 
-def solution(text, ending):
-    position = text.find(ending)
-    if position != -1:
-        if text.count(ending) > 1:
-            for i in range(text.count(ending)-1):
-                position = position + text[position + len(ending):].find(ending) + len(ending)
-        if len(text) > position + len(ending):
-            return False
-        else:
-            return True
-    else:
-        return False
-
-
-def test(text, ending):
-    position = text.find(ending)
-    if text.count(ending) > 1:
-        for i in range(text.count(ending)-1):
-            position = position + text[position + len(ending):].find(ending) + len(ending)
-
-
-print(solution("abcabcabc",  "bc" ))
-
-def square_digits(num):
-    tmp = list(tuple(str(num)))
-    result = ""
-    for i in tmp:
-        result += str(int(i) ** 2)
-
-    return int(result)
+# def solution(text, ending):
+#     position = text.find(ending)
+#     if position != -1:
+#         if text.count(ending) > 1:
+#             for i in range(text.count(ending)-1):
+#                 position = position + text[position + len(ending):].find(ending) + len(ending)
+#         if len(text) > position + len(ending):
+#             return False
+#         else:
+#             return True
+#     else:
+#         return False
+#
+#
+# def test(text, ending):
+#     position = text.find(ending)
+#     if text.count(ending) > 1:
+#         for i in range(text.count(ending)-1):
+#             position = position + text[position + len(ending):].find(ending) + len(ending)
+#
+#
+# print(solution("abcabcabc",  "bc" ))
+#
+# def square_digits(num):
+#     tmp = list(tuple(str(num)))
+#     result = ""
+#     for i in tmp:
+#         result += str(int(i) ** 2)
+#
+#     return int(result)
 
 #print(square_digits(0))
+
+
+# def solution(s):
+#     result = ''
+#     for el in s:
+#         if el.isupper():
+#             print('-'+el+'-')
+#             result += ' '+el
+#         else:
+#             result += el
+#     return result
+#
+#
+# print(solution('helloWorld'))
+
+
+# def pig_it(text):
+#     my_list = text.split()
+#     text = ''
+#     for word in my_list:
+#         if word.isalpha():
+#             tmp = word[1:] + word[0] + 'ay'
+#             text += tmp + ' '
+#         else:
+#             text += word + ' '
+#     return text.strip()
+#
+#
+# print(pig_it('Quis custodiet ipsos custodes ?'))
+
+# def duplicate_encode(word):
+#     my_list = word.lower()
+#     temp = ''
+#     print(my_list)
+#     for char in my_list:
+#         if my_list.count(char) > 1:
+#             temp += ')'
+#         else:
+#             temp += '('
+#         print(char, temp)
+#     return temp
+#
+# print(duplicate_encode("(( @"))
+
+def move_zeros(lst):
+    tmp = []
+    for i, el in enumerate(lst):
+        print(isinstance(el, bool))
+        if el == 0:
+            tmp.append(i)
+    print(tmp)
+    for i in range(len(tmp)-1, -1, -1):
+        lst.pop(tmp[i])
+        lst.append(0)
+    return lst
+
+
+print(move_zeros([9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]))
